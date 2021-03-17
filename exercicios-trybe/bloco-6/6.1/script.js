796e70c1475bab313ec1c30b7c71bc36fa0a5832
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-17 16:35:59 -0300

    exercicio 6.1

---
+++ exercicios-trybe/bloco-6/6.1/script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+
+
+    //local do option
+    const optionLocal = document.getElementById('dropDown');
+
+    function createOption(value, text) {
+        // criando option
+        const option = document.createElement('option');
+
+        option.value = value;
+        option.innerText = text;
+
+        //
+
+        return option;
+
+    }
+
+      
+
+    let sigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+    let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará', 'DistritoFederal', 'EspíritoSanto', 'Goiás', 'Maranhão', 'MatoGrosso', 'MatoGrossoDoSul', 'MinasGerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'RioDeJaneiro', 'RioGrandeDoNorte', 'RioGrandeDoSul', 'Rondônia', 'Roraima', 'SantaCatarina', 'SaoPaulo', 'Sergipe', 'Tocantins'];
+
+    for (let i = 0; i < sigla.length; i += 1) {
+      optionLocal.appendChild( createOption('sigla[i]', 'estados[i]') );    
+
+
+    }
+
+
+
+
+
+
+
+}
