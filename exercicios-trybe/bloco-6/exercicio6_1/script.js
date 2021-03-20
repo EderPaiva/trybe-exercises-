c957326701638633156313abacab6a79ff0a0b60
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-20 10:17:03 -0300

    formulario usando bootstrap

---
+++ exercicios-trybe/bloco-6/exercicio6_1/script.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+
+  // importando biblioteca
+  // let moment = require("moment");
+  // let nascimento = moment('23011994','DD/MM/YYYY');
+  // let myAge = nascimento.fromNow();
+
+  // console.log('Minha idade é:' + fromNow());
+
+
+    //local do option
+    const optionLocal = document.getElementById('dropDown');
+
+    function createOption(value, text) {
+        // criando option
+        const option = document.createElement('option');
+
+        option.value = value;
+        option.innerText = text;
+
+        //
+
+        return option;
+    }
+
+      
+
+    const sigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+    const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará', 'DistritoFederal', 'EspíritoSanto', 'Goiás', 'Maranhão', 'MatoGrosso', 'MatoGrossoDoSul', 'MinasGerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'RioDeJaneiro', 'RioGrandeDoNorte', 'RioGrandeDoSul', 'Rondônia', 'Roraima', 'SantaCatarina', 'SaoPaulo', 'Sergipe', 'Tocantins'];
+
+    for (let i = 0; i < sigla.length; i += 1) {
+  optionLocal.appendChild( createOption(sigla[i], estados[i])) ;   
+
+
+    }
+
+
+
+
+
+
+
+}
+
+let moment = require("moment");
+  let nascimento = moment('23011994','DDMMYYYY');
+  let myAge = nascimento.fromNow();
+console.log('Minha idade é:' + fromNow());
